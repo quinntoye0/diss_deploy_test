7fc23f7e46d33e7d4164c2dc50fbd20654d135ef
Quinn Toye <4toyeq57@solent.ac.uk>
2024-03-12 15:45:08 +0000

    implemented starting point for api and connected to existing client

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
@@ -8,20 +8,55 @@ import Home from './components/pages/Home';
 import SignIn from './components/pages/SignIn';
 import CreateAccount from './components/pages/CreateAccount';
 
-function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' exact element={ <Home />} />
-          <Route path='/sign-in' element={ <SignIn />} />
-          <Route path='/create-account' element={ <CreateAccount />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' exact element={ <Home />} />
+            <Route path='/sign-in' element={ <SignIn />} />
+            <Route path='/create-account' element={ <CreateAccount />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </>
+    );
+  }
+
 }
 
 export default App;
+
+
+    
+// this.Navbar = this.Navbar.bind(this);
+// this.Home = this.Home.bind(this);
+// this.SignIn = this.SignIn.bind(this);
+// this.CreateAccount = this.CreateAccount.bind(this);
+// this.Footer = this.Footer.bind(this);
+
+
+// <div className="App">
+//   <header className="App-header">
+//     <h1 className="App-title">Welcome to React</h1>
+//   </header>
+//   <p className="App-intro">{this.state.apiResponse}</p>
+// </div>
--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from './Button';
 import '../App.css';
 import'./SplashScreen.css'
 
 
 function SplashScreen() {
+
+  const [apiResponse, setApiResponse] = useState('');
+  useEffect(() => {
+    // Fetch API data
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(response => setApiResponse(response));
+  }, []);
+
+
   return (
     <div className='splash-container'>
       {/* <video src='../../public/videos/video-1.mp4' autoPlay loop muted /> */}
       <h1>Reinvent Your Team!</h1>
       <p>It begins here...</p>
+      <p>{apiResponse}</p>
 
       <div className='splash-btns'>
         <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
