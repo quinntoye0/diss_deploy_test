dac3b5a653b8f7f7e0ffa1d83e39f8290f4aaf6a
Quinn Toye <4toyeq57@solent.ac.uk>
2024-04-21 20:07:32 +0100

    fjsdhfkjsdj

--- api/app.js
+++ api/app.js
@@ -17,7 +17,6 @@ module.exports = (req, res) => {
   //set header first to allow request or origin domain (value can be different)
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
 
   //Preflight CORS handler
@@ -34,13 +33,13 @@ module.exports = (req, res) => {
 //   next();
 // });
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');  
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  // res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');  
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   // res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
 // app.options((req, res) => {
 //   res.setHeader('Access-Control-Allow-Origin', "*");
