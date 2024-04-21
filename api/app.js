f3a3288cf5844e42deae1bc03f4b6a3d651b1974
Quinn Toye <4toyeq57@solent.ac.uk>
2024-04-21 20:23:25 +0100

    jjj

--- api/app.js
+++ api/app.js
@@ -22,12 +22,8 @@ const allowCors = fn => async (req, res) => {
   ));
   res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   )
   if (req.method === 'OPTIONS') {
     res.status(200).end()
@@ -41,7 +37,8 @@ const handler = (req, res) => {
   res.end(d.toString())
 }
 
-app.use(allowCors(handler));
+module.exports = allowCors(handler)
+// app.use(allowCors(handler));
 
 // app.use(function (req, res, next) {
 //   res.header('Access-Control-Allow-Origin', 'https://diss-deploy-test-client.vercel.app/');
