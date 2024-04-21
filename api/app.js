3cfec2268a916a5851b622c3b1c1f4b01df6c1ff
Quinn Toye <4toyeq57@solent.ac.uk>
2024-04-21 18:22:22 +0100

    origin null

--- api/app.js
+++ api/app.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors(
     {
-        origin: '*'
+        origin: 'null'
         // methods: ["POST", "GET"],
         // credentials: true
     }
