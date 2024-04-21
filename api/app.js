c5ea9d4447da71f3868a2044a8adb7b332e51375
Quinn Toye <4toyeq57@solent.ac.uk>
2024-04-21 19:49:54 +0100

    hhhh

--- api/app.js
+++ api/app.js
@@ -13,6 +13,21 @@ app.use(cors(
     }
 ));
 
+module.exports = (req, res) => {
+  //set header first to allow request or origin domain (value can be different)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+
+  //Preflight CORS handler
+  if(req.method === 'OPTIONS') {
+      return res.status(200).json(({
+          body: "OK"
+      }))
+  }
+}
+
 // app.use(function (req, res, next) {
 //   res.header('Access-Control-Allow-Origin', 'https://diss-deploy-test-client.vercel.app/');
 //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
