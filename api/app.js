53dd4679bac4510394b18391c196dbb438252f07
Quinn Toye <4toyeq57@solent.ac.uk>
2024-04-21 20:14:08 +0100

    iiii

--- api/app.js
+++ api/app.js
@@ -13,19 +13,29 @@ app.use(cors(
     }
 ));
 
-module.exports = (req, res) => {
-  //set header first to allow request or origin domain (value can be different)
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
-
-  //Preflight CORS handler
-  if(req.method === 'OPTIONS') {
-      return res.status(200).json(({
-          body: "OK"
-      }))
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
   }
+  return await fn(req, res)
 }
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
 
 // app.use(function (req, res, next) {
 //   res.header('Access-Control-Allow-Origin', 'https://diss-deploy-test-client.vercel.app/');
