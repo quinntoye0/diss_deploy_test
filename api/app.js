5c66291a8d09334b3d0055384223379e722480b4
Quinn Toye <4toyeq57@solent.ac.uk>
2024-04-22 14:17:13 +0100

    allowCors 2

--- api/app.js
+++ api/app.js
@@ -10,6 +10,14 @@ const app = express()
 // app.use(cors());
 // app.options("*", cors());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
